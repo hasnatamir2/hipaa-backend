2488a367a8b6610fb7915d813f4cfc5a513ed56e
hasnatamir2 <32700499+hasnatamir2@users.noreply.github.com>
2025-02-05 23:08:37 +0400

    update permission module for notifications, users, files, folders

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,4 +1,4 @@
-// src/permissions/permissions.module.ts
+// permissions/permissions.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PermissionsService } from './permissions.service';
@@ -7,10 +7,14 @@ import { Permission } from './entities/permission.entity/permission.entity';
 import { User } from '../users/entities/user.entity/user.entity';
 import { File } from '../files/entities/file.entity/file.entity';
 import { Folder } from '../folders/entities/folder.entity/folder.entity';
+import { NotificationsModule } from '../notifications/notifications.module'; // Import NotificationsModule here
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permission, User, File, Folder])],
-  controllers: [PermissionsController],
+  imports: [
+    TypeOrmModule.forFeature([Permission, User, File, Folder]), // Inject entities directly
+    NotificationsModule, // Importing NotificationsModule here
+  ],
   providers: [PermissionsService],
+  controllers: [PermissionsController],
 })
 export class PermissionsModule {}
