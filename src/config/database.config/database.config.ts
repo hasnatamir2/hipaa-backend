0da67d94860079a55c33b54dbad14a6d10ba7c82
hasnatamir2 <32700499+hasnatamir2@users.noreply.github.com>
2025-02-08 15:59:58 +0400

    supabase init

--- src/config/database.config/database.config.ts
+++ src/config/database.config/database.config.ts
@@ -1,1 +1,17 @@
-export class DatabaseConfig {}
+// src/config/database.config.ts
+
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const DatabaseConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST') || 'your-supabase-host',
+  port: Number(configService.get<number>('DB_PORT')) || 5432, // Ensuring that it's a number
+  username: configService.get<string>('DB_USERNAME') || 'your-username',
+  password: configService.get<string>('DB_PASSWORD') || 'your-password',
+  database: configService.get<string>('DB_NAME') || 'your-database',
+  synchronize: true, // Make sure to set it to 'false' in production
+  autoLoadEntities: true,
+});
